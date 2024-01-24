6f9a2b70025a27d7d936658447a4161f97a8f85e
JungDaGyeom <ktkt1413@naver.com>
2024-01-24 22:05:51 +0900

    Initial commit

---
+++ src/main/java/com/preorder/preorderservice/PreorderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.preorder.preorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreorderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreorderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/preorder/preorderservice/PreorderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preorder.preorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreorderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
