0964a73aab12270fc4e609ee488bf796482717f4
JungDaGyeom <ktkt1413@naver.com>
2024-02-02 01:43:17 +0900

    validation 유저네임, 비밀번호, 이메일양식 완료

--- src/main/java/com/preorder/preorderservice/controller/UserController.java
+++ src/main/java/com/preorder/preorderservice/controller/UserController.java
@@ -1,21 +1,26 @@
 package com.preorder.preorderservice.controller;
 
-import com.preorder.preorderservice.domain.user.dto.LoginRequestDto;
 import com.preorder.preorderservice.domain.user.dto.SignupRequestDto;
 import com.preorder.preorderservice.domain.user.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
@@ -29,10 +34,19 @@ public class UserController {
         return "signup";
     }
 
-    @PostMapping("user/signup")
-    public String signup(SignupRequestDto requestDto) {
+    @PostMapping("/user/signup")
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
         userService.signup(requestDto);
+
         return "redirect:/api/user/login-page";
     }
-
 }
--- src/main/java/com/preorder/preorderservice/domain/user/dto/SignupRequestDto.java
+++ src/main/java/com/preorder/preorderservice/domain/user/dto/SignupRequestDto.java
@@ -1,14 +1,28 @@
 package com.preorder.preorderservice.domain.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
+    @NotBlank
+    @Size(min = 3, max = 20, message = "유저네임은 최소 3글자 이상 최대 20글자 이하로, 영문 대소문자와 숫자로 이루어져야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$")
     private String username;
+
+    @Size(min = 8, max = 20, message = "비밀번호는 8글자 이상, 대소문자 각 1개, 특수문자 1개, 숫자 1개 이상으로 구성되어야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!#$%&'*+/=?`{|}~^.-])[a-zA-Z\\d!#$%&'*+/=?`{|}~^.-]+$")
+    @NotBlank
     private String password;
+
+    @Pattern(regexp = "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+$")
+    @NotBlank
     private String email;
+
     private boolean admin = false;
     private String adminToken = "";
 }
