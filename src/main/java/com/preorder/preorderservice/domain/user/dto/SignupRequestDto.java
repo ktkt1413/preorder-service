3b6728b2f3abc719cd1615723ddc836f2c03bb57
JungDaGyeom <ktkt1413@naver.com>
2024-02-02 17:12:33 +0900

    회원가입 양식 수정

--- src/main/java/com/preorder/preorderservice/domain/user/dto/SignupRequestDto.java
+++ src/main/java/com/preorder/preorderservice/domain/user/dto/SignupRequestDto.java
@@ -10,11 +10,11 @@ import lombok.Setter;
 @Setter
 public class SignupRequestDto {
     @NotBlank
-    @Size(min = 3, max = 20, message = "유저네임은 최소 3글자 이상 최대 20글자 이하로, 영문 대소문자와 숫자로 이루어져야 합니다.")
+    @Size(min = 4, max = 10, message = "유저네임은 최소 4글자 이상 최대 10글자 이하로, 영문 대소문자와 숫자로 이루어져야 합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9_]+$")
     private String username;
 
-    @Size(min = 8, max = 20, message = "비밀번호는 8글자 이상, 대소문자 각 1개, 특수문자 1개, 숫자 1개 이상으로 구성되어야 합니다.")
+    @Size(min = 8, max = 15, message = "비밀번호는 8글자 이상 최대 15글자 이하로, 대소문자 각 1개, 특수문자 1개, 숫자 1개 이상으로 구성되어야 합니다.")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!#$%&'*+/=?`{|}~^.-])[a-zA-Z\\d!#$%&'*+/=?`{|}~^.-]+$")
     @NotBlank
     private String password;
